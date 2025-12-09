b6b16def7dd80fefd0f8c8900a6b6ca918f8ad5a
gayashan4lk <gayashan4lk@gmail.com>
2025-12-10 00:21:13 +0530

    refactor: simplify authentication UI by removing unused components and enhancing user display

--- components/auth/LoginButton.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-
-export default function LoginButton() {
-	return <a href="/auth/login">Log In</a>
-}
--- components/auth/LogoutButton.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-
-export default function LogoutButton() {
-	return <a href="/auth/logout">Log Out</a>
-}
